2edef7f09a169fe62ed379163d35c8b28cecec5a
Seigo2058 <seigo040508@gmail.com>
2020-06-24 21:27:13 +0900

    デバックモードを有効にする

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ from flask import Flask , render_template , request , redirect , session
 # appにFlaskを定義して使えるようにしています。Flask クラスのインスタンスを作って、 app という変数に代入しています。
 app = Flask(__name__)
 
+
+#ここから上に記述する
+if __name__ == "__main__":
+    #サーバーを起動するよ
+    app.run(debug=True , host="0.0.0.0",port=888)
+    #デバックモードを有効にするよ
+    #192.168.10.62
