9e0db99fcfabc45c1cf881b59c16056865bcefca
yokosan0803 <yoko.san.bb@gmail.com>
2020-06-26 21:18:20 +0900

    ふわっと表示するやつ

--- static/js/common.js
+++ static/js/common.js
@@ -317,9 +317,25 @@ $(function(){
     $('.wark').find('#wark_words3').text(randWark[2]);
 
 
-
-
-
+// ふわっとするやつ--------------------------------------------
+window.onload = function() {
+    scroll_effect();
+  
+    $(window).scroll(function(){
+     scroll_effect();
+    });
+  
+    function scroll_effect(){
+     $('.effect-fade').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight){
+       $(this).addClass('effect-scroll');
+      }
+     });
+    }
+};
 
 
 
